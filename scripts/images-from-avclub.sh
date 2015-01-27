5481430ebbe3e0aa03540374acf5c5930f75af04
Jeff Buchbinder <jeff@ourexchange.net>
2015-01-27 13:29:25 -0500

    Modernize AV Club fetch script.

--- scripts/images-from-avclub.sh
+++ scripts/images-from-avclub.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-if [ $# -ne 1]; then
-	echo "$0 path-to-md-file"
-	echo "Downloads all relevant images from avclub.com"
+SECTION=$1
+SLUG=$2
+
+if [ $# -ne 2 ]; then
+	echo "$0 section slug"
 	exit 1
 fi
 
-cat $1 | grep -iE '^!\[' | cut -d'[' -f2 | cut -d']' -f1 | while read X; do
+if [ ! -d ./content ]; then
+	echo "MUST BE RUN FROM ROOT OF REPOSITORY"
+	exit 1
+fi
+
+B=images/${SECTION}/${SLUG}
+
+mkdir -p $B
+cat content/$SECTION-$SLUG.md | grep -iE '^!\[' | cut -d'[' -f2 | cut -d']' -f1 | while read X; do
 	Y=$( echo $X | cut -c1-4 )
 	Z=$( echo $X | cut -c5-6 )
-	wget -O $X.jpg http://i.onionstatic.com/avclub/$Y/$Z/original/640.jpg
+	wget -O $B/$X.jpg http://i.onionstatic.com/avclub/$Y/$Z/original/640.jpg
 done
 
+git add $B
